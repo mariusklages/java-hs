64f788f661b79517de9715803e47c49434294096
Marius <mariusaklages@gmail.com>
2018-11-13 00:30:17 +0100

    Initial commit

--- src/test/java/space/harbour/java/hw6/MultiThreadCrawlerTest.java
+++ src/test/java/space/harbour/java/hw6/MultiThreadCrawlerTest.java
@@ -1,23 +1,22 @@
+package space.harbour.java.hw6;
+
 import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
-import space.harbour.java.hw6.*;
-
-import static org.junit.Assert.*;
 
 public class MultiThreadCrawlerTest extends TestCase {
 
-    @Before
-    public void setUp() throws Exception {
-        super.setUp();
-    }
+        @Before
+        public void setUp() throws Exception {
+            super.setUp();
+        }
 
-    @Test
-    public void testGetContent() {
-        String[] args = {"https://vasart.github.io/harbour-java-course/page3.html"};
-        MultiThreadCrawler crawler = new MultiThreadCrawler(args);
-        MultiThreadCrawler.Webpage w = crawler.toVisit.getFirst();
-        String text = w.getContentOfWebPage(w.url);
-        assertEquals("<!DOCTYPE html><html><body><h1>Goodbye</h1></body></html>", text);
+        @Test
+        public void testGetContent() {
+            String[] args = {"https://vasart.github.io/harbour-java-course/page3.html"};
+            MultiThreadCrawler crawler = new MultiThreadCrawler(args);
+            MultiThreadCrawler.Webpage w = crawler.toVisit.getFirst();
+            String text = w.getContentOfWebPage(w.url);
+            assertEquals("<!DOCTYPE html><html><body><h1>Goodbye</h1></body></html>", text);
+        }
     }
-}
